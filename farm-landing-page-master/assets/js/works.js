61c3473885a0e76e5d877d4adaae632cbfc7f30e
qharlen <aatanov08@mail.ru>
2024-06-06 19:42:43 +0300

    first commit

---
+++ farm-landing-page-master/assets/js/works.js
@@ -0,0 +1,26 @@
+
+
+export default function works() {
+    const worksSection = document.querySelector('.works__content');
+    const progressbar = worksSection.querySelector('.form_progressbar');
+    const progressbarSteps = progressbar.querySelectorAll('.progressbar__step')
+    const firstStep = worksSection.querySelector('.first_step')
+
+    progressbar.addEventListener('click', (event) => {
+        console.log(1)
+        if (event.target && event.target.nodeName == "LI") {
+            const dataStep = event.target.getAttribute('data-step');
+            for (let index = dataStep - 1; index < progressbarSteps.length; index++) {
+                progressbarSteps[index].classList.remove('active');
+            }
+            for (let index = dataStep - 1; index > 0; index--) {
+                progressbarSteps[index].classList.add('active');
+            }
+            event.target.classList.add('active')
+            firstStep.style.marginLeft = `-${(dataStep-1)*100}%`
+        }
+    });
+}
+
+
+/* End How To Works Section */
